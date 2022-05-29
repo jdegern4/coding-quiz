83350ab36e3d4162f35ff8645e59dfdb89926246
Jordan D <degco44@gmail.com>
2022-05-28 22:19:25 -0500

    add questions

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,9 @@
 const question = document.getElementById("question");
+const quizStartButton = document.getElementById("start-quiz");
 var timer = 60;
 var score = 100;
 var questionNumber = 0;
-var availableQuestions = [];
+var questionSequence = [];
 
 var questions = [
     {
@@ -12,7 +13,7 @@ var questions = [
         ans3: "Document Operator Model",
         ans4: "Document Object Model",
         correct: "4"
-    }
+    },
 
     {
         question: "Where should a CSS file be referenced in an HTML file?",
@@ -21,7 +22,7 @@ var questions = [
         ans3: "Inside the head section",
         ans4: "Inside the body section",
         correct: "3"
-    }
+    },
 
     {
         question: "What is the correct HTML element for inserting a line break?",
@@ -30,11 +31,36 @@ var questions = [
         ans3: "<brk>",
         ans4: "<linebreak>",
         correct: "1"
+    },
+
+    {
+        question: "How do you display hyperlinks without an underline?",
+        ans1: "a {text-decoration:none;}",
+        ans2: "a {underline:none;}",
+        ans3: "a {text-decoration:no-underline;}",
+        ans4: "a {decoration:no-underline;}",
+        correct: "1"
+    },
+
+    {
+        question: "What is the correct syntax for a JavaScript array?",
+        ans1: "var flavors = 1(vanilla), 2(chocolate), 3(strawberry)",
+        ans2: "var flavors = ['vanilla', 'chocolate', 'strawberry']",
+        ans3: "var flavors = (1:'vanilla', 2:'chocolate', 3:'strawberry')",
+        ans4: "var flavors = 'vanilla', 'chocolate', 'strawberry'",
+        correct: "2"
     }
 ]
 
 
 var startQuiz = function() {
-    availableQuestions = [...questions];
-    console.log(availableQuestions);
+    questionSequence = [...questions];
+    console.log(questionSequence);
+    nextQuestion();
 }
+
+var nextQuestion = function() {
+    questionNumber++;
+}
+
+quizStartButton.addEventListener("click", startQuiz);
